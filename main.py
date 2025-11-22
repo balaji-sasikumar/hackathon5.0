f1e857063d4de8c32b0eb661a7f2139cb2227dbc
Surendhar <Surendhar@theproindia.com>
2025-11-22 23:17:32 +0530

    feat: added endpoint to fetch data from mongoDb

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 from interact_with_persona import chat_loop
 from run_persona_survey import run_persona_survey_json
 import uvicorn
-
+from typing import Optional
+from predictor import predict_party
+from pymongo import MongoClient
+import os
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://rishikesh:nzGnd3PK9rhT65OG@tccl-pre-prod-local.u7ngf.mongodb.net/voter-ai?retryWrites=true&w=majority&appName=tccl-pre-prod-local")
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client["voter_ai"]
+collection = db["synthetic_profiles"]
+
+class QueryRequest(BaseModel):
+    constituency: str
 
 class ChatRequest(BaseModel):
     profile: dict
-    question: str
+    question: Optional[str] = None
 
 
 @app.post("/api/chat")
@@ -24,10 +44,36 @@ def interview_persona(req: ChatRequest):
     response = run_persona_survey_json(req.profile)
     return response
 
+@app.post("/api/prediction")
+def predict_party_api(req: dict):
+    predicted_result = predict_party(req)
+    return {"predicted_party": predicted_result}
+
 @app.get("/")
 def read_root():
     return {"message": "FastAPI is running!"}
 
+
+@app.post("/persona/query")
+async def persona_query(req: QueryRequest):
+    profile = fetch_profiles_by_constituency(req.constituency)
+
+    if not profile:
+        raise HTTPException(status_code=404, detail="Constituency not found in MongoDB")
+
+    return {"profile": profile}
+
+def fetch_profiles_by_constituency(constituency: str):
+    cursor = collection.find(
+        {"synthetic_voter_profile.geographic_context.constituency": {"$regex": f"^{constituency}$", "$options": "i"}}
+    )
+    
+    profiles = []
+    for doc in cursor:
+        doc.pop("_id", None)
+        profiles.append(doc)
+
+    return profiles
 
 if __name__ == "__main__":
     uvicorn.run(
--- predictor.py
+++ predictor.py
@@ -4,23 +4,15 @@ import pandas as pd
 model = joblib.load("party_predictor.pkl")
 enc = joblib.load("party_encoder.pkl")
 
-new_person = pd.DataFrame([{
-    "Q1": "A",
-    "Q2": "D",
-    "Q3": "A",
-    "Q4": "C",
-    "Q5": "D",
-    "Q6": "B",
-    "Q7": "A",
-    "Q8": "C",
-    "Q9": "B",
-    "Q10": "D"
-}])
 
-for col in new_person.columns:
-    new_person[col] = new_person[col].map({'A':0,'B':1,'C':2,'D':3})
+def predict_party(responses: dict) -> str:
+    new_person = pd.DataFrame([responses])
 
-prediction = model.predict(new_person)[0]
-party = enc.inverse_transform([prediction])[0]
+    for col in new_person.columns:
+        new_person[col] = new_person[col].map({'A':0,'B':1,'C':2,'D':3})
 
-print("Predicted Party:", party)
+    prediction = model.predict(new_person)[0]
+    party = enc.inverse_transform([prediction])[0]
+
+    print("Predicted Party:", party)
+    return party
